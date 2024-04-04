e2a8357eeaa018b62ab5d533be037861bd7d8ae8
nathaniel mordetzky <njmordetzky@iMac.lan>
2024-04-04 14:00:09 -0700

    adding boilerplate code to existing repo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Nathaniel is Learning React
+          Nathaniel is Learning React today
         </a>
       </header>
     </div>
