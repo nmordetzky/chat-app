c720dbf9e3e4822c8818dd31aa7a1e957db05084
nathaniel mordetzky <njmordetzky@iMac.lan>
2024-04-04 14:20:47 -0700

    connecting local files with github

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Nathaniel is Learning React today
+          Nathaniel is working on Learning React today
         </a>
       </header>
     </div>
