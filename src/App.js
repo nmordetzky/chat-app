3545c635faef9e1a77095e872da28aa4ec4ab4ec
nathaniel mordetzky <njmordetzky@iMac.lan>
2024-04-09 13:28:42 -0700

    initial isAuth and room, state variables and views

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import { Auth } from './components/auth';
+import { useState } from 'react';
+import Cookies from "universal-cookie";
+const cookie = new Cookies(); 
 
 function App() {
+  const [isAuth, setIsAuth] = useState(cookie.get("auth-token"));
+  const [room, setRoom] = useState(null);
+  if (!isAuth) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome to the React Chat App
+          </p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Auth />
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +29,16 @@ function App() {
           Welcome to the React Chat App
         </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <Auth />
+        <div> {room ? (
+                <div>Chat</div> 
+              ) : (
+                <div> 
+                  <label>Enter a Chat Room Name</label>
+                  <input class="room-name"/>
+                  <button>Join Chat</button>
+                </div>
+              )}
+        </div>
       </header>
     </div>
   );
