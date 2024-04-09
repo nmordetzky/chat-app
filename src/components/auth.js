870d61c6c2ec990a5cf0e54020337fe6e86cb69a
nathaniel mordetzky <njmordetzky@iMac.lan>
2024-04-08 17:41:49 -0700

    importing universal cookie and setting user info with it

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,10 +1,15 @@
 import { auth, provider } from "../firebase-config.js";
 import { signInWithPopup } from "firebase/auth";
 
+import Cookies from "universal-cookie";
+const cookie = new Cookies(); 
+
 export const Auth = () => {
 
     const signInWithGoogle = async () => {
-        await signInWithPopup(auth, provider);
+        const result = await signInWithPopup(auth, provider);
+        console.log(result);
+        cookie.set("auth-token", result.user.refreshToken);
     };
 
     return (
